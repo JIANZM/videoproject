39c77b148478dbf8421d585b2d7687ad0271f9a4
ZongmingJian <1264569735@qq.com>
2019-05-01 12:23:16 +0800

    优化配置

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y-i+dj&m@dd7@@unus5xe%r^c&_o8_yleev+70#+m9dv)a+res'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.100.25']
 
